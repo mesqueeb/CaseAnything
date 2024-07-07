97ede08830be4566de79096fafdb3fe9ad1962e7
mesqueeb <mesqueeb@users.noreply.github.com>
2024-07-07 15:01:04 +0900

    chore: lint

--- Package.swift
+++ Package.swift
@@ -9,14 +9,18 @@ let package = Package(
     // Products define the executables and libraries a package produces, making them visible to other packages.
     .library(
       name: "CaseAnything",
-      targets: ["CaseAnything"]),
+      targets: ["CaseAnything"]
+    ),
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
     // Targets can depend on other targets in this package and products from dependencies.
     .target(
-      name: "CaseAnything"),
+      name: "CaseAnything"
+    ),
     .testTarget(
       name: "CaseAnythingTests",
-      dependencies: ["CaseAnything"]),
-  ])
+      dependencies: ["CaseAnything"]
+    ),
+  ]
+)
